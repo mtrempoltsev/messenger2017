c91e458c82bb76e46caf363bfde1c2f40ff0ea79
FakeEmperor <fake_empire@mail.ru>
2017-08-05 13:38:06 +0300

    :fire: Merged m2::crypto into shared/

--- client/core/tests/net-interop/server/m2/mock_srv.py
+++ client/core/tests/net-interop/server/m2/mock_srv.py
@@ -8,7 +8,7 @@ from aiohttp import web
 
 from kily.net.helpers import HandlerEntry
 from kily.net.server import Server
-from kily.system.core import SystemDelegate, Module
+from kily.system.core import SystemDelegate, Module, ModuleStatus
 from m2.routes.registration import RegistrationProcessModule
 
 
@@ -19,10 +19,11 @@ class MockServer(Server):
         super().__init__()
         self._registration = None  # type: RegistrationProcessModule
 
-    def load(self, system: 'SystemDelegate', **deps: 'Module'):
-        super().load(system, **deps)
+    def load(self, system: 'SystemDelegate', **deps: 'Module') -> ModuleStatus:
         self._registration = deps[RegistrationProcessModule.NAME]
+        super().load(system, **deps)
         self._context.load_context_named("db", {})  # make db in AppContext
+        return self._status
 
     def get_handlers(self) -> Iterable[HandlerEntry]:
         return [
@@ -33,12 +34,4 @@ class MockServer(Server):
         aiohttp_debugtoolbar.setup(self._server)
         super().run(**kwargs)
 
-    async def handle_register_intent(self, req: web.Request) -> web.Response:
-        return web.Response(status=403, reason="GOVNO KOLYANA")
-
-    async def handle_registration_challenge(self, req: web.Request) -> web.Response:
-        return web.Response(status=403, reason="GOVNO KOLYANA")
-
-    async def handle_register(self, req: web.Request) -> web.Response:
-        return web.Response(status=403, reason="GOVNO KOLYANA")
 
