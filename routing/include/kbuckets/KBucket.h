0e370b724e987f46811278fdbd7f3aeb300a24eb
abogovski <abogovski@gmail.com>
2017-08-05 16:21:49 +0300

    Implement KBucket (#8)
    * Use m2::routing namespace
    
    * Implement KBucket

--- routing/include/kbuckets/KBucket.h
+++ routing/include/kbuckets/KBucket.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "../data_structures/NodeInfo.h"
 
 namespace m2 {
 namespace routing {
-        
+
+class KBucket final
+{
+public:
+    KBucket();
+    
+    bool insert(const m2::routing::NodeInfo& src);
+    void replaceOldest(const m2::routing::NodeInfo& src);
+
+    NodeInfo oldest() const;
+    
+    std::list<m2::routing::NodeInfo> known() const { return nodes; }
+    size_t knownCnt() const { return m_size; }
+
+protected:
+    std::list<m2::routing::NodeInfo> nodes;
+    size_t filled; // NOTE: list::size O(capacity) implementation is allowed 
+    size_t k;
+};
+    
 } // namespace routing
 } // namespace m2
--- routing/src/kbuckets/KBucket.cpp
+++ routing/src/kbuckets/KBucket.cpp
@@ -1,7 +1,41 @@
-#include "kbuckets/KBucket.h"
+#include "KBucket.h"
+#include <stdexcept>
 
 namespace m2 {
 namespace routing {
-        
+    
+KBucket::KBucket():
+    k(16) // load from config instead
+{}
+
+bool KBucket::insert(const m2::routing::NodeInfo& src)
+{
+    if (filled < k) {
+       nodes.push_back(src);
+       ++filled;
+       return true;
+    }
+    
+    return false;
+}
+
+void KBucket::replaceOldest(const m2::routing::NodeInfo& src)
+{
+    if (!filled) {
+        throw std::runtime_error("KBucket::replaceOldest: empty bucket");
+    }
+
+    nodes.back() = src;
+}
+
+NodeInfo KBucket::oldest() const
+{
+    if (!filled) {
+        throw std::runtime_error("KBucket::Oldest: empty bucket");
+    }
+
+    return nodes.back();
+}
+
 } // namespace routing
 } // namespace m2
