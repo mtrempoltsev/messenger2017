24024aa4e911c3c0b4523360904582957893f7d8
Anastasiya Simonenko <avsimonenko@esc-center.ru>
2017-08-05 17:45:33 +0300

    Implement insert method in KBucketsManager (with some caps)

--- routing/include/kbuckets/KBucketsManager.h
+++ routing/include/kbuckets/KBucketsManager.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <map>
+#include <list>
+#include <algorithm>
+
 #include "data_structures/NodeInfo.h"
 #include "kbuckets/KBucket.h"
+#include "kbuckets/KBucketsTools.h"
+#include "utils/Config.h"
 
 namespace m2 {
 namespace routing {
@@ -17,7 +23,11 @@ public:
     void getNeighbours(const NodeInfo &nodeInfo) const;
 
 private:
-//    std::map<int, KBucket> intervalToBucket;
+    std::map<int, KBucket> intervalToBucket;
+
+    NodeInfo ourNodeInfo;
+
+    int getIntervalInMap(int xorResult);
 
 };
         
--- routing/src/kbuckets/KBucketsManager.cpp
+++ routing/src/kbuckets/KBucketsManager.cpp
@@ -1,23 +1,76 @@
+#include <algorithm>
 #include "kbuckets/KBucketsManager.h"
 
 namespace m2 {
 namespace routing {
 
 
-    KBucketsManager::KBucketsManager(const NodeInfo &nodeInfo) {
+    KBucketsManager::KBucketsManager(const NodeInfo &nodeInfo)
+    : ourNodeInfo(nodeInfo)
+    {
+        KBucket initialKBucket;
+        initialKBucket.insert(nodeInfo);
+        intervalToBucket.insert(std::pair<int, KBucket>(0, initialKBucket));
+    }
+
+    void KBucketsManager::insert(const NodeInfo &newNodeInfo) {
+//        int xorResult = KBucketsTools::calculateXor(ourNodeInfo, newNodeInfo); //TODO
+        int xorResult = 1; //some power of 2
+        int bucketIndex = getIntervalInMap(xorResult);
+
+        if (bucketIndex != -1) //think about situation when bucketIndex == -1
+        {
+            auto bucket = intervalToBucket[bucketIndex];
+
+            auto bucketList = bucket.known(); //TODO replace by KBucket method
+            bool found = (std::find(bucketList.begin(), bucketList.end(), newNodeInfo) != bucketList.end());
+            if (found) {
+                //TODO call KBucket::moveToTop(newNodeInfo)
+                return;
+            }
+
+            int K = Config::getK();
+            if (bucket.knownCnt() < K) {
+                bucket.insert(newNodeInfo);
+            } else { //bucket is full
+                //TODO replace by KBucket method
+                bool foundOurs = (std::find(bucketList.begin(), bucketList.end(), ourNodeInfo) != bucketList.end());
+                if (foundOurs) { //this bucket contains ourNodeInfo
+                    //TODO split
+                    return;
+                } else {
+                    auto lastConnectedNodeInfo = bucket.oldest();
+                    //TODO ping lastConnectedNodeInfo, do appropriate actions when it would answer on ping or not
+                }
+
+            }
+        }
+    }
+
+    void KBucketsManager::getNeighbours(const NodeInfo &nodeInfo) const
+    {
 
     }
 
-    void KBucketsManager::insert(const NodeInfo &nodeInfo) {
+    KBucketsManager::KBucketsManager()
+    { //TODO remove it later, it's just for build
 
     }
 
-    void KBucketsManager::getNeighbours(const NodeInfo &nodeInfo) const {
+    int KBucketsManager::getIntervalInMap(int xorResult)
+    {
+        std::list<int> smallerKeys;
+        std::copy_if(intervalToBucket.begin(), intervalToBucket.end(),
+                                        std::back_inserter(smallerKeys),
+                     [xorResult](std::pair<int, KBucket> keyToValue) {
+                         return keyToValue.first < xorResult;
+                     });
 
-    }
+        if (smallerKeys.size() == 0) //actually we shouldn't face this situation...
+            return -1;
 
-    KBucketsManager::KBucketsManager() {
-
+        auto result = std::max_element(smallerKeys.begin(), smallerKeys.end());
+        return *result;
     }
 
 } // namespace routing
