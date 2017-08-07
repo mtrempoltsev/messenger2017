cd47a3a3a1a2c586494779cadf6784240f0fe56c
Dmitry Bragin <dmbragin@sc.esc-center.ru>
2017-08-07 20:21:13 +0300

    Write login controler with placeholder

--- client/gui/include/controlers/logincontroler.h
+++ client/gui/include/controlers/logincontroler.h
@@ -3,15 +3,32 @@
 
 #include <QObject>
 
+namespace m2 {
+namespace gui {
+namespace controler {
+
+
 class LoginControler : public QObject
 {
     Q_OBJECT
 public:
     explicit LoginControler(QObject *parent = nullptr);
 
+    Q_INVOKABLE void login() const;
+
+    void loginSuccessed(const std::string &guid);
+    void loginFailed();
+
 signals:
+    void startLogin() const;
+    void finishLoginSuccessed() const;
+    void finishLoginFailed(const QString &msg) const;
 
 public slots:
 };
+
+}
+}
+}
 
 #endif // LOGINCONTROLER_H
