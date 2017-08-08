981d2d0b35fc0326e7bb12e265410153e4752b7d
Neilana <neilana@yandex.ru>
2017-08-08 15:46:12 +0000

    handlers.h comments

--- client/shared/handlers.h
+++ client/shared/handlers.h
@@ -2,6 +2,39 @@
 
 //#include <list>
 
+/*
+  Привет из кора!
+  Где-то в main() будет что-то типа такого:
+
+  m2::core::Core core;                    // стартуем ядро
+  m2::core::CoreDispatcher dispatcher;    // стартуем диспетчер (к - кэп)
+
+  Ну а потом вы будете вызывать нас через этот самый диспетчер примерно так:
+
+  void zaregaiMenyaKnopka(){
+    RegisterHandler handler;
+    handler.onCompletion = []() { emit registrationOk(); };
+    handler.onError = []() { emit vsePropalo(); };
+    dispatcher.RegisterUser(handler);
+  }
+
+  Тут будет общая струткура того, что мы вам возвращаем.
+
+  Ещё есть core/include/core_dispatcher.h, где описаны эти самые функции,
+  которые вы вызываете через диспатчер. Пока они принимают только хэндлеры, но
+  вангую что будут ситуации, когда вы захотите нам что-то передать (или я не
+  права?)
+
+  Пока у нас не предусмотрено ничего, кроме следующего:
+
+  void Login(LoginHandler handler);
+  void RegisterUser(RegisterHandler handler);
+  void GetServerSet(ServerSetHandler handler);
+
+  В принципе, уже сейчас вы можете нас вызвыать, и мы вам вернём, что у нас всё
+  хорошо :D
+ */
+
 namespace m2 {
 
 struct LoginHandler final {
@@ -20,6 +53,7 @@ struct RegisterHandler final {
   ErrorHandler onError;
 };
 
+// В процессе...
 struct ServerSetHandler final {
   using CompletionHandler = std::function<void(std::set<std::string> servers)>;
   using ErrorHandler = std::function<void(Error &&error)>;
