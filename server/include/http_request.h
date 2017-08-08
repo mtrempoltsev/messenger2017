3d3e2ade37d2f56735db7fc954736fa9b2c2940d
Maksim Sergeev <m.sergeev@corp.mail.ru>
2017-08-08 21:37:22 +0300

    fix

--- server/include/http_request.h
+++ server/include/http_request.h
@@ -2,7 +2,6 @@
 
 #include <iostream>
 
-#include <boost/lexical_cast.hpp>
 #include <map>
 #include <memory>
 
--- server/include/manager_controller.h
+++ server/include/manager_controller.h
@@ -2,7 +2,7 @@
 
 #include "http_request.h"
 #include "http_response.h"
-
+#include "Data/stdafx.h"
 
 namespace m2     {
 namespace server {
@@ -22,7 +22,7 @@ namespace server {
         Database* db;
         Session* session_;
 
-        uuids::uuid userUid {0};
+        uuids::uuid userUid;
     };
 
 
